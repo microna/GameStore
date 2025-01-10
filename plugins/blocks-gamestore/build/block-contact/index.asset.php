0fb0abe51e2cf9cac73813c51303f12b20089c16
microna <kievonfire@gmail.com>
2025-01-10 14:41:02 +0200

    plugin init

---
+++ plugins/blocks-gamestore/build/block-contact/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5bf88704be905ac7f1a2');
---
+++ plugins/core-gamestore/core-gamestore.php
@@ -0,0 +1,12 @@
+<?php
+/**
+Plugin Name: Core plugin
+Author: Stas Shok
+Version: 1.0
+Description: This is not just a core plugin
+
+Text Domain: core-gamestore
+*/
+
+define('GAMESTORE_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('GAMESTORE_PLUGIN_PATH', plugin_dir_path(__FILE__));
